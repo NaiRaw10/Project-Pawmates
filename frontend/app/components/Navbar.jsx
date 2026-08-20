07eaf56d8696e9abe941bac58f6f38bf060f9d2f
JihanSanddw <jihansanddw@gmail.com>
2026-08-20 22:35:22 +0700

    feat: menambahkan struktur navbar

---
+++ frontend/app/components/Navbar.jsx
@@ -0,0 +1,20 @@
+export default function Navbar() {
+  return (
+    <nav className="">
+        <div className="">
+            Pawmates
+        </div>
+
+        <div className="">
+            <a href="#">Beranda</a>
+            <a href="#">Tentang Kami</a>
+            <a href="#">Galeri</a>
+            <a href="#">Kontak</a>
+        </div>
+
+        <a href="#" className="">
+            Gabung Sekarang
+        </a>
+    </nav>
+  );
+}
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,7 +1,20 @@
+import Navbar from "./components/Navbar";
+
 export default function Home() {
   return (
-    <main>
-      <h1>Pawmates</h1>
-    </main>
+    <>
+      <Navbar />
+
+      <main
+        id="home"
+        className="min-h-screen bg-[#F6F0E6] pt-32"
+      >
+        <div className="flex min-h-[80vh] items-center justify-center">
+          <h1 className="text-4xl font-bold text-[#351B14]">
+            Pawmates
+          </h1>
+        </div>
+      </main>
+    </>
   );
 }
