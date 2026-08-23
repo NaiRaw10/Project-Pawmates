4e978d1440e6e929b5f7c515f447ae1e175d22bf
JihanSanddw <jihansanddw@gmail.com>
2026-08-23 18:33:02 +0700

    feat: menambahkan container utama CTA

---
+++ frontend/app/components/Contact.jsx
@@ -0,0 +1,11 @@
+export default function JoinCommunity() {
+  return (
+    <section
+      id="join"
+      className="px-6 py-16 sm:px-10 sm:py-24"
+    >
+      <div className="rounded-[30px] bg-maroon p-7 text-white sm:p-10 md:p-14">
+      </div>
+    </section>
+  );
+}
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,3 +1,4 @@
+import JoinCommunity from "./components/Contact";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
@@ -7,6 +8,8 @@ export default function Home() {
       <Navbar />
 
       <Hero></Hero>
+
+      <JoinCommunity></JoinCommunity>
     </>
   );
 }
