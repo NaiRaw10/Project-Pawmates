e3b0b32103a89084dd595da549dde11496ececfb
JihanSanddw <jihansanddw@gmail.com>
2026-09-01 07:49:50 +0700

    feat: menambahkan brand dan info footer

---
+++ frontend/app/components/Footer.jsx
@@ -0,0 +1,23 @@
+export default function Footer() {
+  return (
+    <footer
+      id="contact"
+      className="bg-[#2F1710] px-10 py-14 text-white"
+    >
+      <div className="grid gap-10 md:grid-cols-4">
+        <div>
+          <h2 className="text-xl font-bold text-gold">
+            Pawmates
+          </h2>
+
+          <p className="mt-4 text-sm leading-relaxed text-gray-300">
+            Komunitas pecinta anjing terbesar di Indonesia.
+            Berbagi kasih sayang, edukasi, dan kebersamaan
+            lewat sahabat berbulu kita.
+          </p>
+        </div>
+
+      </div>
+    </footer>
+  );
+}
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
@@ -7,6 +8,8 @@ export default function Home() {
       <Navbar />
 
       <Hero></Hero>
+
+      <Footer></Footer>
     </>
   );
 }
