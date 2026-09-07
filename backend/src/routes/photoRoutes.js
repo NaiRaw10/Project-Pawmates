1670408534fba9536e5eb872ff2bac6b8bc3a2c4
JihanSanddw <jihansanddw@gmail.com>
2026-09-08 00:07:44 +0700

    feat: add photo api route

--- backend/src/routes/photoRoutes.js
+++ backend/src/routes/photoRoutes.js
@@ -2,4 +2,10 @@ const express = require("express");
 
 const router = express.Router();
 
+const {
+  getPhotos,
+} = require("../controllers/photoController");
+
+router.get("/", getPhotos);
+
 module.exports = router;
