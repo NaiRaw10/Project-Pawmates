f584d06a3ac8697f0b300716b09093a2721fc243
JihanSanddw <jihansanddw@gmail.com>
2026-09-08 00:04:34 +0700

    feat: up contact api route

--- backend/src/routes/contactRoutes.js
+++ backend/src/routes/contactRoutes.js
@@ -2,4 +2,10 @@ const express = require("express");
 
 const router = express.Router();
 
+const {
+  createContact,
+} = require("../controllers/contactController");
+
+router.post("/", createContact);
+
 module.exports = router;
